e121d39e59eb8c41dfd4144e20c71db7e3eff326
lordmilko <lordmilko@users.noreply.github.com>
2016-12-27 00:33:07 +1100

    Updated PrtgAPI.sln

---
+++ PrtgAPI/PowerShell/Cmdlets/Incomplete/GetSensorHistory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrtgAPI.PowerShell.Cmdlets.Incomplete
+{
+    class Record
+    {
+        public DateTime DateTime { get; set; }
+        public string ChannelName { get; set; }
+        public int ChannelId { get; set; }
+        public string Value { get; set; } 
+    }
+
+    [Cmdlet(VerbsCommon.Get, "SensorHistory")]
+    public class GetSensorHistory : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            var list = new List<Record>();
+
+            list.Add(new Record
+            {
+                DateTime = DateTime.Now,
+                ChannelName = "Percent Available Memory",
+                ChannelId = 1,
+                Value = "100%"
+            });
+
+            WriteObject(list, true);
+        }
+    }
+}
---
+++ PrtgAPI/PowerShell/Cmdlets/Incomplete/NewTriggerParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using PrtgAPI.Parameters;
+
+namespace PrtgAPI.PowerShell.Cmdlets.Incomplete
+{
+    [Cmdlet(VerbsCommon.New, "TriggerParameter")]
+    public class NewTriggerParameter : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            //WriteObject(new StateTriggerParameters())
+        }
+    }
+}
