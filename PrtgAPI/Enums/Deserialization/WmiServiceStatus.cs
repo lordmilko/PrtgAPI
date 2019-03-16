e9dc21f3e360061fdfbc03bcdd05c013cbfb20f7
lordmilko <lordmilko@users.noreply.github.com>
2019-03-16 11:25:07 +1100

    Fixed incorrect XmlEnum value on WmiServiceStatus values StopPending, ContinuePending and PausePending (For #64)

--- PrtgAPI/Enums/Deserialization/WmiServiceStatus.cs
+++ PrtgAPI/Enums/Deserialization/WmiServiceStatus.cs
@@ -8,43 +8,43 @@ namespace PrtgAPI
     public enum WmiServiceStatus
     {
         /// <summary>
-        /// The service is stopped. This corresponds to the Win32 SERVICE_STOPPED constant, which is defined as 0x00000001.
+        /// The service is stopped. This corresponds to the Win32 <see langword="SERVICE_STOPPED" /> constant, which is defined as 0x00000001.
         /// </summary>
         [XmlEnum("Stopped")]
         Stopped = 1,
 
         /// <summary>
-        /// The service is starting. This corresponds to the Win32 SERVICE_ constant, which is defined as 0x00000002.
+        /// The service is starting. This corresponds to the Win32 <see langword="SERVICE_START_PENDING" /> constant, which is defined as 0x00000002.
         /// </summary>
         [XmlEnum("Start Pending")]
         StartPending = 2,
 
         /// <summary>
-        /// The service is stopping. This corresponds to the Win32 SERVICE_ constant, which is defined as 0x00000003.
+        /// The service is stopping. This corresponds to the Win32 <see langword="SERVICE_STOP_PENDING" /> constant, which is defined as 0x00000003.
         /// </summary>
-        [XmlEnum("StopPending")]
+        [XmlEnum("Stop Pending")]
         StopPending = 3,
 
         /// <summary>
-        /// The service is running. This corresponds to the Win32 SERVICE_ constant, which is defined as 0x00000004.
+        /// The service is running. This corresponds to the Win32 <see langword="SERVICE_RUNNING" /> constant, which is defined as 0x00000004.
         /// </summary>
         [XmlEnum("Running")]
         Running = 4,
 
         /// <summary>
-        /// The service continue is pending. This corresponds to the Win32 SERVICE_ constant, which is defined as 0x00000005.
+        /// The service continue is pending. This corresponds to the Win32 <see langword="SERVICE_CONTINUE_PENDING" /> constant, which is defined as 0x00000005.
         /// </summary>
-        [XmlEnum("ContinuePending")]
+        [XmlEnum("Continue Pending")]
         ContinuePending = 5,
 
         /// <summary>
-        /// The service pause is pending. This corresponds to the Win32 SERVICE_ constant, which is defined as 0x00000006.
+        /// The service pause is pending. This corresponds to the Win32 <see langword="SERVICE_PAUSE_PENDING" /> constant, which is defined as 0x00000006.
         /// </summary>
-        [XmlEnum("PausePending")]
+        [XmlEnum("Pause Pending")]
         PausePending = 6,
 
         /// <summary>
-        /// The service is paused.. This corresponds to the Win32 SERVICE_ constant, which is defined as 0x00000007.
+        /// The service is paused.. This corresponds to the Win32 <see langword="SERVICE_PAUSED" /> constant, which is defined as 0x00000007.
         /// </summary>
         [XmlEnum("Paused")]
         Paused = 7
