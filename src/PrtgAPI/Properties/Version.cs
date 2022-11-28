b20a5dedf389be020aca06c00718ab65479c81d3
lordmilko <lordmilko@users.noreply.github.com>
2022-11-28 17:19:43 +1100

    PrtgAPI 0.9.18

--- src/PrtgAPI/Properties/Version.cs
+++ src/PrtgAPI/Properties/Version.cs
@@ -4,5 +4,5 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.17.0")]
-[assembly: AssemblyInformationalVersion("0.9.17")]
+[assembly: AssemblyFileVersion("0.9.18.0")]
+[assembly: AssemblyInformationalVersion("0.9.18")]
